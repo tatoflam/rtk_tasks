ee296a25c175b0a7ff85c72230b9fd73662691d7
Tatsuro Homma <tato.flam@gmail.com>
2022-08-10 16:56:44 +0900

    finalize react_redux_tasks study

---
+++ src/features/login/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Header.module.css";
+import { useSelector } from "react-redux";
+import { selectProfile } from "./loginSlice";
+
+const Header = () => {
+  const profile = useSelector(selectProfile);
+  console.log(profile);
+
+  return (
+    <div className={styles.header}>
+      <h3>{profile.username}</h3>
+      <h1>Today's task</h1>
+    </div>
+  );
+};
+
+export default Header;
